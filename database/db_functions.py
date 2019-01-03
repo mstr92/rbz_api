348fed64e84b32aa19bd8729c3028ae28195352a
Herbert Fuchs <herbert.fuchs@student.tugraz.at>
2019-01-03 20:20:24 +0100

    changed something

--- database/db_functions.py
+++ database/db_functions.py
@@ -42,8 +42,7 @@ def get_entry(id):
 
 def get_genre(id):
     try:
-        db.session.commit()
-        print("test")
+        #db.session.commit()
         return GenreModel.query.filter(GenreModel.id == id).one()
     except exc.SQLAlchemyError:
         print("No entry in Database")
