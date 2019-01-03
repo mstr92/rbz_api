f0201201ecd858cdc073110844b6ff1745931f5c
Herbert Fuchs <herbert.fuchs@student.tugraz.at>
2019-01-03 21:29:34 +0100

    changed something

--- endpoints/movies.py
+++ endpoints/movies.py
@@ -109,14 +109,14 @@ class BotTest(Resource):
         modelObject = get_genre(1)
         print(modelObject.genrename)
         print(modelObject.id)
-        return "lalal"
-        # # Check if object in database
-        # if modelObject == None:
-        #     return None, 404
-        #
-        # # Check if object response is set
-        # if modelObject.genrename == None:
-        #     return "No Genre detected", 405
-        # else:
-        #     return modelObject.genrename, 201
+
+        # Check if object in database
+        if modelObject == None:
+            return None, 404
+
+        # Check if object response is set
+        if modelObject.genrename == None:
+            return "No Genre detected", 405
+        else:
+            return modelObject.genrename, 201
         #
