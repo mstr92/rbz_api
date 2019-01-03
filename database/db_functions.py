4214c815e0e02ad5949d211a5ff6f893aff5b63a
Herbert Fuchs <herbert.fuchs@student.tugraz.at>
2019-01-03 21:19:01 +0100

    changed something

--- database/db_functions.py
+++ database/db_functions.py
@@ -42,7 +42,7 @@ def get_entry(id):
 
 def get_genre(id):
     try:
-        db.session.commit()
+        engine = create_engine(SQLALCHEMY_DATABASE_URI)
         #return GenreModel.query.filter(GenreModel.id == id).one()
         return engine.execute("SELECT id, genrename FROM genre WHERE id = %s", (str(id)))
     except exc.SQLAlchemyError:
