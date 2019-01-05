48fed2f718e7c966025f8bb8ee4d87713fad94a1
Herbert Fuchs <herbert.fuchs@student.tugraz.at>
2019-01-05 19:37:40 +0100

    changed something

--- database/db_functions.py
+++ database/db_functions.py
@@ -149,6 +149,7 @@ def set_user_device_id(username, deviceId):
         if userModel == None:
            return 401
         else:
+            print(userModel.deviceID)
             if len(userModel.deviceID) > 0:
                 splitUUID = userModel.deviceID.split(';')
                 if len(filter(lambda x: x == deviceId, splitUUID)) > 0:
