df41bc5734d825e3cd552e63d3a4441a2d6c5898
Herbert Fuchs <herbert.fuchs@student.tugraz.at>
2019-01-05 19:20:15 +0100

    changed something

--- database/db_functions.py
+++ database/db_functions.py
@@ -146,12 +146,12 @@ def set_user_device_id(username, deviceId):
     try:
         userModel = UserModel.query.filter(UserModel.username == username).first()
         if userModel == None:
-
+            engine = create_engine(SQLALCHEMY_DATABASE_URI)
+            engine.execute("UPDATE user SET deviceID = CONCAT(IFNULL(deviceID,''), %s ) WHERE username = %s",
+                           (deviceId + ';', username))
+        else:
             splitUUID = userModel.deviceID.split(';')
             console.log(splitUUID)
-        else:
-            engine = create_engine(SQLALCHEMY_DATABASE_URI)
-            engine.execute("UPDATE user SET deviceID = CONCAT(IFNULL(deviceID,''), %s ) WHERE username = %s", (deviceId + ';' , username ))
         return 201
     except exc.SQLAlchemyError as e:
         print("No entry in Database")
